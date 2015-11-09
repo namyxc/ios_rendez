23e8859b2f61a8c9db9f6293b067b06fb4f0ddd8
rentit <rentit@rentits-macbook-pro-xcode-6.local>
2015-11-09 16:10:33 +0100

    Initial Commit

---
+++ rendez/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  rendez
+//
+//  Created by rentit on 2015. 11. 09..
+//  Copyright © 2015. rentit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ rendez/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  rendez
+//
+//  Created by rentit on 2015. 11. 09..
+//  Copyright © 2015. rentit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
